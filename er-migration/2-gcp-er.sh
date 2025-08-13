c644f8c5295c99cf2517db9bbda74565f08e72ef
Daniel Mauser <dmauser@hotmail.com>
2025-08-13 10:26:02 -0500

    update migration lab

--- er-migration/2-gcp-er.sh
+++ er-migration/2-gcp-er.sh
@@ -1,7 +1,7 @@
 # Define your variables
 project=angular-expanse-327722 #Set your project Name. Get your PROJECT_ID use command: gcloud projects list 
-region=us-central1 #Set your region. Get Regions/Zones Use command: gcloud compute zones list
-zone=us-central1-c # Set availability zone: a, b or c.
+region=us-east1 #Set your region. Get Regions/Zones Use command: gcloud compute zones list
+zone=us-east1-b # Set availability zone: a, b or c.
 vpcrange=192.168.100.0/24
 envname=er-migration
 vmname=vm1
@@ -16,7 +16,7 @@ gcloud compute networks subnets create $envname-subnet --range=$vpcrange --netwo
 gcloud compute firewall-rules create $envname-allow-traffic-from-azure --network $envname-vpc --allow tcp,udp,icmp --source-ranges 192.168.0.0/16,10.0.0.0/8,172.16.0.0/12,35.235.240.0/20,$mypip/32
 
 #Create Unbutu VM:
-gcloud compute instances create $envname-vm1 --zone=$zone --machine-type=f1-micro --network-interface=subnet=$envname-subnet,network-tier=PREMIUM --image=ubuntu-1804-bionic-v20220126 --image-project=ubuntu-os-cloud --boot-disk-size=10GB --boot-disk-type=pd-balanced --boot-disk-device-name=$envname-vm1 
+gcloud compute instances create $envname-vm1 --zone=$zone --machine-type=f1-micro --network-interface=subnet=$envname-subnet,network-tier=PREMIUM --image-family=ubuntu-2204-lts --image-project=ubuntu-os-cloud --boot-disk-size=10GB --boot-disk-type=pd-balanced --boot-disk-device-name=$envname-vm1 
 #gcloud compute instances create $envname-vm1 --zone=$zone --machine-type=f1-micro --network-interface=subnet=$envname-subnet,network-tier=PREMIUM --image-family=ubuntu-1804-lts-arm64 --image-project=ubuntu-os-cloud --boot-disk-size=10GB --boot-disk-type=pd-balanced --boot-disk-device-name=$envname-vm1 
 
 #Cloud Router: #***********Validate************
